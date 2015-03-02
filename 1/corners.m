6a492a2aa57ab7f3c7958a1322efafa0c03cc649
simplay <michael.single@students.unibe.ch>
2015-03-02 18:14:14 +0100

    Initial version

---
+++ 1/corners.m
@@ -0,0 +1,62 @@
+% Harris Corner Detection Algorithm:
+%   steps:
+%   1. Compute Image gradients using a Sobel Kernel
+%   2. Smooth results
+%        Sx2 = g * Ix.^2
+%        Sy2 = g * Iy.^2
+%        Sxy = g * Ix.*Iy
+%   3. H(x,y) = [Ix^2 Ix*Iy; Ix*Iy Iy^2]
+%   R = det(H) - k tr^2(H)
+%   threshold on value of R
+%   Compute nonmax suppression
+%
+% implementation is based on theory from 
+%   www.cse.psu.edu/~rcollins/CSE486/lecture06.pdf
+%
+% @param img given image we want to detect corner 100x100 in our case.
+% @param sigma std usually equal 1.
+% @param lowerBound float. a corner candidate must be bigger than this
+% bound. Only significant maximal values should be identified as an corner.
+% @param radius Integer window base radius (usually 1).
+% @return rowIdxs array of row indices in image of maxima (corner)
+% @return columnIdxs array of column indices in image of maxima (corner)
+%
+function [rowIdxs, columnIdxs] = corners(img, sigma, lowerBound, radius)
+    
+    % sobel derivative kernel
+    dx = [-1 0 1; -1 0 1; -1 0 1]; 
+    dy = dx';
+    
+    % Image derivatives
+    Ix = conv2(img, dx, 'same');    
+    Iy = conv2(img, dy, 'same');    
+
+    % Gaussian filter of size 6*sigma (since std +/- 3sigma ~99.99%)
+    % fix(...) => round to nearest integer.
+    g = fspecial('gaussian', max(1,fix(6*sigma)), sigma);
+    
+    % Smoothed squared image derivatives
+    Sx2 = conv2(Ix.^2, g, 'same'); 
+    Sy2 = conv2(Iy.^2, g, 'same');
+    Sxy = conv2(Ix.*Iy, g, 'same');
+    
+    % found by testing.
+    k=0.06;
+    
+    % response of detector: det(H) - k(trace(H)^2)
+    R = (Sx2.*Sy2 - Sxy.^2) - k*(Sx2 + Sy2).^2; 
+
+    % neithborhood window size - common assumption twice radius recentered (+1).
+	windowLength = (2*radius+1);
+	
+    % visit a windowLength^2 in R and find max in this neighborhood. 
+    % replace set at each pixel location the found max. neighborhood value.
+    % i.e. the nonmax supression
+    localMaxima = ordfilt2(R, windowLength^2, ones(windowLength));
+    
+    % consider only max. values that are bigger than a given minimal
+    % threshold boundary. I.e. count only real maxima. Find indices of
+    % significant local maxima.
+	[rowIdxs, columnIdxs] = find((R==localMaxima)&(R>lowerBound));   
+end
+	
---
+++ 1/demo.m
@@ -0,0 +1,50 @@
+% shape recognition
+clc
+clear all;
+close all;
+
+img = imread('Input/shapes0.png');
+img = ~img;
+img = im2double(img);
+
+
+% Image img is (M x N)
+[M,N] = size(img); 
+
+% Traverse Imgae: image consists of 100 x 100 pixels subimages:
+slice = ones(100,1);
+solution = textread('Input/Shapes0.txt','%s');
+
+idx = 1;
+equalityCount = 0;
+% iterate over all subimages and classify.
+for m=1:100:M,
+    for n=1:100:N,
+        counter = 0;
+        subimage = img(m:m+99, n:n+99);
+        
+        % preprocess image - pre-smooth it => good results
+        G = fspecial('gaussian');
+        subimage = imfilter(subimage,G,'same');
+        
+        % retrieve edges using a harris corner detector.
+        [~,c] = corners(subimage, 1.0, 0.5, 1);
+        shape = '';
+        if length(c) == 0,
+            shape = 'circle';
+        elseif length(c) == 3
+            shape = 'triangle';
+        elseif length(c) == 4,
+            shape = 'square';
+        else
+            shape = 'star';
+        end
+        
+        current_solution = strjoin(solution(idx));
+        equalityCount = equalityCount + strcmp(current_solution, shape);
+        disp([num2str(idx), '. ' ,shape, '<=>' ,current_solution, ' ' , num2str(strcmp(current_solution, shape))])
+        idx = idx + 1;
+    end
+end
+disp(['equaltity count: ',num2str(equalityCount)]);
+
