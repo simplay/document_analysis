71547126bf28155fb9cb88f56e82ff7a9577f2a6
simplay <michael.single@students.unibe.ch>
2015-03-17 13:43:01 +0100

    Extended docs of getCornerParameters

--- 1/getCornerParameters.m
+++ 1/getCornerParameters.m
@@ -1,6 +1,6 @@
 function [ threshold ] = getCornerParameters( preprocessingFlag, fringeRanking)
 %GETCORNERPARAMETERS get threshold boundary for given issue case
-%   values have been experimentally determined.
+%   values have been experimentally determined (by try and error).
 %   @param preprocessingFlag 
 % it is equal to:
 %       1 if Gaussian noise detected
@@ -12,6 +12,12 @@ function [ threshold ] = getCornerParameters( preprocessingFlag, fringeRanking)
         threshold = 0.5;
     elseif preprocessingFlag == 4
         threshold = 0.19;
+        % heuristics: From comparisons between many data-sets we
+        % noticed that low-fringe examples exhibit a ranking 450 < r < 600
+        % usually. Being a low fringe mean that we have a higher lower
+        % bound for later corner detection. Idally, this value would be
+        % linearly interpolated between 0.19 and 0.22 instead of applying
+        % this kind of clipping scheme.
         if (fringeRanking > 450 && fringeRanking < 600)
             threshold = 0.22;
         end     
