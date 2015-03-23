bed0e055c48b80eca26dc82f89259be3886f618b
simplay <michael.single@students.unibe.ch>
2015-03-23 19:01:47 +0100

    Skeletton of segmentation approach for extracting text lines

---
+++ 2/alternative_sl_approach.m
@@ -0,0 +1,16 @@
+%% Initialization
+
+% assuming we are in '../2/' 
+addpath('../vlfeat');
+addpath('src');
+
+% install dependencies:
+run ../vlfeat/toolbox/vl_setup
+
+%% Flush all previous generated outputs/data
+clear all;
+close all;
+clc;
+
+
+
---
+++ 2/src/imageSegmentation.m
@@ -0,0 +1,7 @@
+function [ output_args ] = imageSegmentation( input_args )
+%IMAGESEGMENTATION Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
