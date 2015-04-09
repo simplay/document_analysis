547551261a9576739e34a8a1ab61a17a2947d3b9
panmari <smmuzi@gmail.com>
2015-04-09 16:53:12 +0200

    Fixed convex hull to work in all cases.

--- 2/convex_hull_bw.m
+++ 2/convex_hull_bw.m
@@ -2,14 +2,15 @@ function [ union_convex_bw, x_coords, y_coords ] = convex_hull_bw( bw )
 %CONVEX_HULL_BW Takes a bw image and creates a convex hull around all white
 %pixels. Returned as a bw image.
 % DOES NOT WORK IF THERE IS A HORIZONTAL GAP between the white pixels.
-
-[row, ~] = find(bw, 1, 'first');
-[row_last, ~] = find(bw, 1, 'last');
+%keyboard;
+[row, ~] = find(bw);
+row_first = min(row);
+row_last = max(row);
 x_coords = [];
 y_coords = [];
 union_convex_bw = zeros(size(bw));
 
-for k = row:row_last
+for k = row_first:row_last
     [~, min_curr] = find(bw(k,:), 1, 'first');
     [~, max_curr] = find(bw(k, :), 1, 'last');
     union_convex_bw(k, min_curr:max_curr) = 1;
