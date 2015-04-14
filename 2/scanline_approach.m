27a9a651622b2b57ce0ef71cb73a5a966df45963
panmari <smmuzi@gmail.com>
2015-04-14 18:02:15 +0200

    Deciding now addaptively on binarization threshold for profile analysis.
    Depending on bins. Lots of bins.

--- 2/scanline_approach.m
+++ 2/scanline_approach.m
@@ -1,17 +1,23 @@
-function [line_img ] = scanline_approach(img)     
+function [line_img ] = scanline_approach(img)   
+    %% Make profile analysis
     % white = fg.
     sum_rows = sum(img, 1);
     sum_columns = sum(img, 2);
-
-    % Convert to binary by thresholding sum values according to mean. 
-    threshold_factor_rows = -1;
-    threshold_factor_cols = 0.4;
-    binary_rows = sum_rows > mean(sum_rows)*threshold_factor_rows;
-    binary_columns = sum_columns > mean(sum_columns)*threshold_factor_cols;
+    sum_rows_normed = sum_rows/max(sum_rows);
+    sum_columns_normed = sum_columns/max(sum_columns);
+    [h, centers] = hist(sum_columns_normed(sum_columns_normed > 0.01 & sum_columns_normed < 0.5));
+    h_grad = h(1:end - 1) - h(2:end);
+    [~, max_grad_idx] = max(h_grad);
+    threshold_factor_cols = centers(max_grad_idx + 1);
+    % Convert to binary by thresholding.
+    % (rows are ignored bc of threshold 0)
+    threshold_factor_rows = 0;
+    binary_rows = sum_rows_normed >= threshold_factor_rows;
+    binary_columns = sum_columns_normed >= threshold_factor_cols;
     
     %% Kill smaller runs of 1, set them to 0
-    binary_columns =  eliminate_runs(binary_columns, 1, 5);
-    binary_columns = eliminate_runs(binary_columns, 0, 5);
+    binary_columns =  eliminate_runs(binary_columns, 1, 4);
+    binary_columns = eliminate_runs(binary_columns, 0, 4);
 
     %% Turn into masks
     row_binary_img = repmat(binary_rows, [size(img, 1), 1]);
