35876d22cf636bb268ef33bcbb4540c396c00f22
panmari <smmuzi@gmail.com>
2015-04-21 09:09:00 +0200

    Improved computeSimilarities
    Does now not change size in every iteration.

--- 3/computeSimilarities.m
+++ 3/computeSimilarities.m
@@ -1,9 +1,9 @@
 function similarities = computeSimilarities(histograms, compare_hist, nrImages)
-    similarities = [];
+    similarities = zeros(nrImages, 2);
     for img=1:nrImages
         sim = sum(histograms(:,img).*compare_hist)/...
             (norm(histograms(:,img) * norm(compare_hist)));
-        similarities = [similarities; sim img];
+        similarities(img, :) = [sim img];
     end
     similarities = sortrows(similarities);
     similarities = flipud(similarities);
