33a86b9be73e61529e88c39922d96e00b46e4b88
panmari <smmuzi@gmail.com>
2015-04-21 17:19:21 +0200

    Renamed some variables, made draft of precision/recall plot.

--- 3/draw_tpr_fpr_graph.m
+++ 3/draw_tpr_fpr_graph.m
@@ -5,7 +5,8 @@ function [hit_words, tpr, fpr] = draw_tpr_fpr_graph(query_word, gt_strings, simi
     non_keywords_in_manuscript = length(gt_strings) - keywords_in_manuscript;
     correct_count = 0;
     wrong_count = 0;
-    correct_incorrect = zeros(2, length(similarities));
+    true_positives = zeros(1, length(similarities));
+    false_positives = zeros(1, length(similarities));
     hit_words = {length(similarities)};
     for i=1:length(similarities)
         hit_idx = similarities(i, 2);
@@ -16,14 +17,23 @@ function [hit_words, tpr, fpr] = draw_tpr_fpr_graph(query_word, gt_strings, simi
         else
             wrong_count = wrong_count + 1;
         end
-        correct_incorrect(:, i) = [correct_count wrong_count];
+        true_positives(i) = correct_count;
+        false_positives(i) = wrong_count;
     end
 
-    tpr = correct_incorrect(1, :) ./ keywords_in_manuscript;
-    fpr = correct_incorrect(2, :) ./ non_keywords_in_manuscript;
+    tpr = true_positives ./ keywords_in_manuscript;
+    fpr = false_positives ./ non_keywords_in_manuscript;
     plot(fpr, tpr);
     xlabel('False positive rate');
     ylabel('True positive rate');
     title(query_word)
+    
+    recall = tpr;
+    precision = true_positives ./ ...
+        (true_positives + false_positives);
+    plot(recall, precision, '.');    
+    xlabel('Recall');
+    ylabel('Precision');
+    title(query_word);
 end
 
