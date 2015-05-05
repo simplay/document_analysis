fe1c2abe7cb335edec425a2d7058c20653a22278
simplay <michael.single@students.unibe.ch>
2015-05-05 21:48:05 +0200

    Responding progress

--- 4/dump_features_for.m
+++ 4/dump_features_for.m
@@ -3,19 +3,19 @@ function [ ] = dump_features_for( folder, output_file )
 % to the given output file. The correct label is prepended in the beginning.
 % Features are computed using the function compute_features(img)
 
-train_files = dir([folder '*.png']);
-for file_number=1:length(train_files)
-    file = train_files(file_number);
-    [~, filename] = fileparts(file.name);
-    split_filename = strsplit(filename, '-');
-    lbl = split_filename(2);
-    lbl_number = lbl{1}(end);
-    features = compute_features(imread([folder, file.name]));
-    fprintf(output_file, '%s,', lbl_number);
-    % TODO: possibly get rid of trailing comma.
-    fprintf(output_file, '%f,', features);
-    fprintf(output_file, '\n');
-end
+    train_files = dir([folder '*.png']);
+    for file_number=1:length(train_files)
+        file = train_files(file_number);
+        [~, filename] = fileparts(file.name);
+        split_filename = strsplit(filename, '-');
+        lbl = split_filename(2);
+        lbl_number = lbl{1}(end);
+        features = compute_features(imread([folder, file.name]));
+        fprintf(output_file, '%s,', lbl_number);
+        % TODO: possibly get rid of trailing comma.
+        fprintf(output_file, '%f,', features);
+        fprintf(output_file, '\n');
+    end
 
 end
 
--- 4/preprocessing.m
+++ 4/preprocessing.m
@@ -3,11 +3,11 @@ test_folder = 'input/images/test/';
 
 train_output = fopen('mnist.train.txt', 'w');
 test_output = fopen('mnist.test.txt', 'w');
-
+disp('Start Preprocessing...');
 tic
 dump_features_for(train_folder, train_output);
 dump_features_for(test_folder, test_output);
 toc
-
+disp('Finished Preprocessing..');
 fclose(train_output);
 fclose(test_output);
