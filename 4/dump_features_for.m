09209f2828fba82ce2b5dc0bbe126b05459a4d4e
simplay <michael.single@students.unibe.ch>
2015-05-05 21:59:03 +0200

    Some minor docs about file structure

--- 4/dump_features_for.m
+++ 4/dump_features_for.m
@@ -7,11 +7,16 @@ function [ ] = dump_features_for( folder, output_file )
     for file_number=1:length(train_files)
         file = train_files(file_number);
         [~, filename] = fileparts(file.name);
+        
+        % file name convention example: 
+        %   'img'    'lbl0'    '1'    'num'
         split_filename = strsplit(filename, '-');
+
         lbl = split_filename(2);
         lbl_number = lbl{1}(end);
         features = compute_features(imread([folder, file.name]));
         fprintf(output_file, '%s,', lbl_number);
+        
         % TODO: possibly get rid of trailing comma.
         fprintf(output_file, '%f,', features);
         fprintf(output_file, '\n');
