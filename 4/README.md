0c7ae6b98cab520a82e560e7507c656a6a46ca70
simplay <michael.single@students.unibe.ch>
2015-05-05 17:43:14 +0200

    Updated README and allow to run app as ./

--- 4/README.md
+++ 4/README.md
@@ -1,5 +1,7 @@
 # Exercise 4 README
 
+## Installation and Usage
+
 1. Download the [MNIST dataset](https://dl.dropboxusercontent.com/u/663533/da_ex4/mnist.zip).
 2. Extract the files of the archieve `mnist.zip` into `input/`.
-3. run `ruby start.rb`
+3. Run `ruby start.rb`
--- 4/start.rb
+++ 4/start.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/env ruby
+
 matlab_file = "preprocessing.m"
 command = "java -jar -Xmx512m nn.jar -a SIGMOID -f 784 -n 100 -0 10 -l 0.001 -e 10 mnist.train.txt mnist.test.txt mnist.train.output.txt mnist.test.output.txt"
 run_matlab = "matlab -nodisplay -nosplash -nodesktop -r \"run('${PWD}/#{matlab_file}'); quit\""
