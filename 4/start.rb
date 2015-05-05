f72c144f29f54e0e91bcefaf7122eabad86aa256
simplay <michael.single@students.unibe.ch>
2015-05-05 17:34:45 +0200

    Renamed matlab file

--- 4/start.rb
+++ 4/start.rb
@@ -1,4 +1,4 @@
-matlab_file = "dummy.m"
+matlab_file = "preprocessing.m"
 command = "java -jar -Xmx512m nn.jar -a SIGMOID -f 784 -n 100 -0 10 -l 0.001 -e 10 mnist.train.txt mnist.test.txt mnist.train.output.txt mnist.test.output.txt"
 run_matlab = "matlab -nodisplay -nosplash -nodesktop -r \"run('${PWD}/#{matlab_file}'); quit\""
 system(run_matlab)
