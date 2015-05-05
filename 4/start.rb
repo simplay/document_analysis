437c4a4e99c14b113e28a9a74c05ec14e8f91749
simplay <michael.single@students.unibe.ch>
2015-05-05 17:29:32 +0200

    Run matlab preprocessing from start script before running nn.jar

--- 4/start.rb
+++ 4/start.rb
@@ -1,2 +1,5 @@
+matlab_file = "dummy.m"
 command = "java -jar -Xmx512m nn.jar -a SIGMOID -f 784 -n 100 -0 10 -l 0.001 -e 10 mnist.train.txt mnist.test.txt mnist.train.output.txt mnist.test.output.txt"
+run_matlab = "matlab -nodisplay -nosplash -nodesktop -r \"run('${PWD}/#{matlab_file}'); quit\""
+system(run_matlab)
 system(command)
