035344c44ce01613cee8750156c5ce48bbeadcdc
panmari <smmuzi@gmail.com>
2015-05-05 13:20:36 +0200

    Producing now output for handin.

--- 3/draw_tpr_fpr_graph.m
+++ 3/draw_tpr_fpr_graph.m
@@ -1,5 +1,6 @@
 function [hit_words, tpr, fpr] = ...
-    draw_tpr_fpr_graph(query_word, gt_strings, similarities)
+    draw_tpr_fpr_graph(set_name, query_word, gt_strings, ...
+    gt_filenames, similarities, draw_figures, is_week2)
 %DRAW_TPR_FPR_GRAPH Draws various graphs for evaluation of the results.
 %   @query_word, a string of the current query word. Should correspond to
 %   its representation in gt_strings
@@ -22,6 +23,12 @@ function [hit_words, tpr, fpr] = ...
     % Go over all similarities descendingly, compare with ground truth and
     % increment correct_count resp. wrong_count. Also computes true/false
     % positives (when pulling n-images).
+    if is_week2
+        subfolder = 'week2/';
+    else
+        subfolder = 'week1/';
+    end
+    output = fopen(['Output/' subfolder set_name '_' query_word '.txt' ], 'w');
     for i=1:length(similarities)
         hit_idx = similarities(i, 2);
         hit_word = gt_strings{hit_idx};
@@ -35,36 +42,39 @@ function [hit_words, tpr, fpr] = ...
         end
         true_positives(i) = correct_count;
         false_positives(i) = wrong_count;
+        fprintf(output, '%s\n', gt_filenames{hit_idx});
     end
-    hFig = figure;
-    %set(hFig, 'Position', [0 0 1000 400])
-    %s = subplot(1,2,1);
     tpr = true_positives ./ keywords_in_manuscript;
     fpr = false_positives ./ non_keywords_in_manuscript;
-    plot(fpr, tpr);
-    xlabel('False positive rate');
-    ylabel('True positive rate');
-    [~, equal_error_rate_idx] = min(abs(1 - tpr - fpr));
-    title(sprintf('%s, ROC curve, %d occurences', query_word, ...
-       keywords_in_manuscript));
-    saveas(hFig, sprintf('%s_sent_roc', query_word),'png');
+    if draw_figures
+        hFig = figure;
+        %set(hFig, 'Position', [0 0 1000 400])
+        %s = subplot(1,2,1);
+        plot(fpr, tpr);
+        xlabel('False positive rate');
+        ylabel('True positive rate');
+        [~, equal_error_rate_idx] = min(abs(1 - tpr - fpr));
+        title(sprintf('%s, ROC curve, %d occurences', query_word, ...
+           keywords_in_manuscript));
+        saveas(hFig, sprintf('%s_sent_roc', query_word),'png');
 
-    %s = subplot(1,2,2);
-    hFig = figure;
-    recall = tpr;
-    precision = true_positives ./ ...
-         (true_positives + false_positives);
-    F_one = 2 * recall .* precision ./ (precision + recall);
-    F_one_max = max(F_one);
-    % Compute area below graph:
-    average_precision = trapz(recall,precision);
-    plot(recall, precision);    
-    xlabel('Recall');
-    ylabel('Precision');
-    axis([0 1 0 1]);
-    title(sprintf('%s, Recall-precision', query_word));
-    fprintf('%s: tpr@EER=%f, AP=%f\n', ...
-        query_word, tpr(equal_error_rate_idx), average_precision);
-    saveas(hFig, sprintf('%s_sent_rp', query_word),'png');
+        %s = subplot(1,2,2);
+        hFig = figure;
+        recall = tpr;
+        precision = true_positives ./ ...
+             (true_positives + false_positives);
+        F_one = 2 * recall .* precision ./ (precision + recall);
+        F_one_max = max(F_one);
+        % Compute area below graph:
+        average_precision = trapz(recall,precision);
+        plot(recall, precision);    
+        xlabel('Recall');
+        ylabel('Precision');
+        axis([0 1 0 1]);
+        title(sprintf('%s, Recall-precision', query_word));
+        fprintf('%s: tpr@EER=%f, AP=%f\n', ...
+            query_word, tpr(equal_error_rate_idx), average_precision);
+        saveas(hFig, sprintf('%s_sent_rp', query_word),'png');
+    end
 end
 
