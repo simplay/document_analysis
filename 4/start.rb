75717ed6f886e1b2a6de5f78659ff224ea2129b9
simplay <michael.single@students.unibe.ch>
2015-05-05 22:28:31 +0200

    Dynamic feature size detection

--- 4/start.rb
+++ 4/start.rb
@@ -2,6 +2,11 @@
 
 require 'optparse'
 
+file = File.new("mnist.train.txt", "r")
+line = file.gets
+feature_size = line.split(/,/).count-1
+file.close
+
 user_args = {}
 opt_parser = OptionParser.new do |opt|
   opt.banner = "Usage example: ruby start.rb -p 1 to run with Matlab preprocessing"
@@ -20,7 +25,7 @@ rescue OptionParser::MissingArgument
 end
 
 matlab_file = "preprocessing.m"
-command = "java -jar -Xmx512m nn.jar -a SIGMOID -f 784 -n 100 -o 10 -l 0.001 -e 10 mnist.train.txt mnist.test.txt mnist.train.output.txt mnist.test.output.txt"
+command = "java -jar -Xmx512m nn.jar -a SIGMOID -f #{feature_size} -n 100 -o 10 -l 0.001 -e 10 mnist.train.txt mnist.test.txt mnist.train.output.txt mnist.test.output.txt"
 run_matlab = "matlab -nodisplay -nosplash -nodesktop -r \"run('${PWD}/#{matlab_file}'); quit\""
 system(run_matlab) if user_args[:preprocess] == 1
 system(command)
