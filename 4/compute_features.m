86bbef1200d208c1ae9b9c3fae27cd0063b79548
panmari <smmuzi@gmail.com>
2015-05-07 14:09:43 +0200

    Added dummy feature for testing.
    Amount of ink for every row/column and normalized to [-1, 1]

--- 4/compute_features.m
+++ 4/compute_features.m
@@ -1,7 +1,13 @@
 function [ features ] = compute_features( img )
-%COMPUTE_FEATURES Summary of this function goes here
-%   Detailed explanation goes here
-
-    features = [1 1 1];
+%COMPUTE_FEATURES Computes a feature vector for the given image.
+    grayscale_img = rgb2gray(img);
+    %[w, h] = size(grayscale_img);
+    w = 28;
+    range = (w * 255)/2;
+    features_scaled = [sum(grayscale_img) sum(grayscale_img, 2)'];
+    % Normalize to range [-1, 1]
+    % Normalization is the same for sum along rows and columns, since
+    % images are all square.
+    features = (features_scaled - range) / range ;
 end
 
