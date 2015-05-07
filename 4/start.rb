933acf03c6e48796137d11114960a48656c03748
panmari <smmuzi@gmail.com>
2015-05-07 14:07:43 +0200

    Checking number of features now AFTER computing features
    And added some code for error handling if input file missing.

--- 4/start.rb
+++ 4/start.rb
@@ -2,11 +2,6 @@
 
 require 'optparse'
 
-file = File.new("mnist.train.txt", "r")
-line = file.gets
-feature_size = line.split(/,/).count-1
-file.close
-
 user_args = {}
 opt_parser = OptionParser.new do |opt|
   opt.banner = "Usage example: ruby start.rb -p 1 to run with Matlab preprocessing"
@@ -24,8 +19,18 @@ rescue OptionParser::MissingArgument
   exit
 end
 
+# Run preprocessing.
 matlab_file = "preprocessing.m"
-command = "java -jar -Xmx512m nn.jar -a SIGMOID -f #{feature_size} -n 100 -o 10 -l 0.001 -e 10 mnist.train.txt mnist.test.txt mnist.train.output.txt mnist.test.output.txt"
 run_matlab = "matlab -nodisplay -nosplash -nodesktop -r \"run('${PWD}/#{matlab_file}'); quit\""
 system(run_matlab) if user_args[:preprocess] == 1
+
+# Determine number of features retrieved in preprocessing.
+begin
+    file = File.open("mnist.train.txt", "r")
+    line = file.gets
+    feature_size = line.split(',').count-1
+    file.close
+rescue
+    raise 'Somethings wrong with input file, try to run with argument -p 1 to generate features'
+end
 system(command)
