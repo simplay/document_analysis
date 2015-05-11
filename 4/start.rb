e68a7ad20effe1e86a24038d901836ed02089d0b
simplay <michael.single@students.unibe.ch>
2015-05-11 16:51:13 +0200

    Do not recompute existing files by default

--- 4/start.rb
+++ 4/start.rb
@@ -6,7 +6,7 @@ require_relative 'prepare_training_data.rb'
 user_args = {}
 opt_parser = OptionParser.new do |opt|
   opt.banner = "Usage example: ruby start.rb -p 1 to run with Matlab preprocessing"
-opt.separator ""
+  opt.separator ""
   opt.on("-p", "--preprocess g", Integer, "Preprocessing mode: Runs preprocessing if 1 otherwise not.") do |preprocess|
     user_args[:preprocess] = preprocess
   end
@@ -27,12 +27,12 @@ system(run_matlab) if user_args[:preprocess] == 1
 
 # Determine number of features retrieved in preprocessing.
 begin
-    file = File.open("mnist.train.txt", "r")
-    line = file.gets
-    feature_size = line.split(',').count-1
-    file.close
+  file = File.open("mnist.train.txt", "r")
+  line = file.gets
+  feature_size = line.split(',').count-1
+  file.close
 rescue
-    raise 'Somethings wrong with input file, try to run with argument -p 1 to generate features'
+  raise 'Somethings wrong with input file, try to run with argument -p 1 to generate features'
 end
 
 training_samples = [20, 100, 1000, 2000, 5000, 10000]
@@ -54,7 +54,11 @@ parameters_list.each do |parameters|
   training_data_file_idx = parameters[3]
   puts "Parameters: nn=#{nn} e=#{e} lr=#{lr} training_data_file_idx=#{training_data_file_idx}"
   filename = "a_SIGMOID_f_#{feature_size}_n_#{nn}_o_10_l_#{lr}_e_#{e}_td_#{training_data_file_idx}.txt"
-  command = "java -jar -Xmx512m nn.jar -a SIGMOID -f #{feature_size} -n #{nn} -o 10 -l #{lr} -e #{e} mnist.train.#{training_data_file_idx}.txt mnist.test.txt mnist.train.output.txt mnist.test.output.txt > #{filename}"
-  system(command)
+  unless File.exist?(filename)
+    command = "java -jar -Xmx512m nn.jar -a SIGMOID -f #{feature_size} -n #{nn} -o 10 -l #{lr} -e #{e} mnist.train.#{training_data_file_idx}.txt mnist.test.txt mnist.train.output.txt mnist.test.output.txt > #{filename}"
+    system(command)
+  else
+    puts "Skipping file #{filename} - it already exists!"
+  end
 end
 
