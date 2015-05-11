6312a0f863c5d4068767a2851aec43d518d979ef
simplay <michael.single@students.unibe.ch>
2015-05-11 17:10:26 +0200

    Fixed broken trainingdata sets input data generation

--- 4/prepare_training_data.rb
+++ 4/prepare_training_data.rb
@@ -1,17 +1,24 @@
+require 'pry'
 class PrepareTraningData
   # @param filename [String] trainingdata file name.
   def initialize(filename, samples)
-    file = File.open(filename)
+    file = File.new(filename, 'r')
     file_line_count = file.each_line.count
     take_idx_multiple = (file_line_count / samples).to_i
     training_data_filename = "mnist.train.#{take_idx_multiple}.txt"
-    out_file = File.new(training_data_filename, "w")
-    file.each_line.select.with_index do |line, line_idx|
+    collected_lines = []
+    file = File.new(filename, 'r')
+    file.lines.select.with_index do |line, line_idx|
       if (line_idx % take_idx_multiple == 0)
-        out_file.puts(line)
+        collected_lines << line
       end
     end
     file.close
+
+    out_file = File.new(training_data_filename, "w")
+    collected_lines.each do |collected_line|
+      out_file.puts(collected_line)
+    end
     out_file.close
   end
 end
