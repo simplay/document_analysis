6b079bfe00a8ed777fb17fb2891cf2a1f65eabf4
simplay <michael.single@students.unibe.ch>
2015-05-12 15:40:53 +0200

    Checkstyle: autoformat

--- 4/visualize.rb
+++ 4/visualize.rb
@@ -9,19 +9,19 @@ g.title = "Learning rate impact, #epochs=#{nr_epochs}"
 learning_rates = %w(0.1 0.01 0.001 0.0001)
 
 learning_rates.each do |lr|
-	evaluation_files = Dir["a_*_l_#{lr}_e_#{nr_epochs}.txt"]
-	results = []
+  evaluation_files = Dir["a_*_l_#{lr}_e_#{nr_epochs}.txt"]
+  results = []
   evaluation_files.sort_by! { |ele| ele.split('n_').last.split('_').first.to_i }
   puts evaluation_files.join(', ')
   evaluation_files.each do |file|
-		File.open(file) do |f|
-			f.each_line do |line|
-				row = line.split /\s/
-				if row[0] == 'accuracy:'
-					results << row[1].to_f
-				end
-			end
-		end
+    File.open(file) do |f|
+      f.each_line do |line|
+        row = line.split /\s/
+        if row[0] == 'accuracy:'
+          results << row[1].to_f
+        end
+      end
+    end
   end
   puts results.join(',')
   g.data lr, results
