1ae79a59fc9b467a779a00cba67732f4ebbf1084
panmari <smmuzi@gmail.com>
2015-05-26 10:27:07 +0200

    Hacked together more images.

--- 4/visualize.rb
+++ 4/visualize.rb
@@ -3,13 +3,15 @@ require 'rmagick'
 g = Gruff::Line.new
 g.y_axis_label = 'Accuracy'
 g.x_axis_label = '#Neurons'
-g.labels = { 0 => '5', 1 => '10', 2 => '50', 3 => '100', 4 => '200' }
-nr_epochs = 50
-g.title = "Learning rate impact, #epochs=#{nr_epochs}"
-learning_rates = %w(0.1 0.01 0.001 0.0001)
+g.labels = { 0 => '5', 1 => '10', 2 => '50', 3 => '100', 4 => '200', 5 => '500', 6 => '784', 7 => '1000', 8 => '2*784' }
+nr_epochs = 100
+#learning_rates = %w(0.1 0.01 0.001 0.0001)
+lr = 0.1
+g.title = "Training set size impact \n #epochs=#{nr_epochs}, learning rate=#{lr}"
 
-learning_rates.each do |lr|
-  evaluation_files = Dir["a_*_l_#{lr}_e_#{nr_epochs}.txt"]
+tds = %w(1 6 12 30 60 600 3000)
+tds.each do |td|
+  evaluation_files = Dir["output/a_*_l_#{lr}_e_#{nr_epochs}_td_#{td}.txt"]
   results = []
   evaluation_files.sort_by! { |ele| ele.split('n_').last.split('_').first.to_i }
   puts evaluation_files.join(', ')
@@ -24,7 +26,8 @@ learning_rates.each do |lr|
     end
   end
   puts results.join(',')
-  g.data lr, results
+  label = (60000 / td.to_i).to_s
+  g.data label, results
 end
 file_path = 'output/evaluation'
-g.write("#{file_path}/learning_rates.png")
+g.write("#{file_path}/training_set_size_e_#{nr_epochs}_lr_#{lr}.png")
